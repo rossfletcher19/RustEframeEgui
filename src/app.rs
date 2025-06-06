ecb1d1ddb56eb9946027135e4dd5ccae5b7c3de4
RossF <rtbfletch@outlook.com>
2025-06-05 17:14:20 -0700

    add lots of stuff, areas, images, code display

--- src/app.rs
+++ src/app.rs
@@ -4,17 +4,25 @@
 pub struct TemplateApp {
     // Example stuff:
     label: String,
+    counter: i32,
+    name_input: String,
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+    x: i32,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            /*
+            Example stuff:
+            */
+            label: "Hello Worldddd!!!".to_owned(),
+            value: 3.5,
+            counter: 0,
+            name_input: String::new(),
+            x: 5,
         }
     }
 }
@@ -32,6 +40,19 @@ impl TemplateApp {
         }
 
         Default::default()
+    }
+
+    fn increment(&mut self) {
+        self.counter += 1;
+    }
+
+    fn reset(&mut self) {
+        self.counter = 0;
+    }
+
+    fn printx(&mut self) {
+        let x = self.x;
+        println!("the value of x is {}", x);
     }
 }
 
@@ -66,8 +87,20 @@ impl eframe::App for TemplateApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
+            // use egui_extras::syntax_highlighting::{highlight, CodeTheme};
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            // let theme = CodeTheme::from_style(ui.style());
+            // let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
+            //     highlight(ui.ctx(), &egui::Style, &theme, "rs") // "rs" for Rust
+            //         .layout_job
+            //         .wrap_width(wrap_width)
+            //         .into()
+            // };
+            ui.heading("rust_eframe_egui template weeeeaaoeu");
+
+            egui::Frame::new().fill(egui::Color32::RED).show(ui, |ui| {
+                ui.label("Label with red background");
+            });
 
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
@@ -81,10 +114,96 @@ impl eframe::App for TemplateApp {
 
             ui.separator();
 
+            egui::Area::new(egui::Id::new("code_area1"))
+            .default_pos(egui::pos2(100.0, 100.0))
+            .show(ctx, |ui| {
+                ui.label("Here is some example code:");
+
+                let code = r#"
+fn main() {
+    println!("Hello, Rust!");
+}
+"#;
+
+                ui.add(
+                    egui::TextEdit::multiline(&mut code.to_string())
+                        .font(egui::TextStyle::Monospace) // Use monospaced font
+                        .code_editor() // Enables code styling
+                        .desired_rows(6)
+                        .lock_focus(true)
+                        .desired_width(400.0),
+                );
+            });
+
+        egui::Area::new(egui::Id::new("my_area2"))
+            .movable(true)
+            .default_pos(egui::pos2(50.0, -400.0))
+            .show(ctx, |ui| {
+                egui::Frame::new() // Start with no built-in padding/frame
+                    .stroke(egui::Stroke::new(1.0, egui::Color32::ORANGE)) // border width and color
+                    .fill(egui::Color32::from_gray(30)) // optional background fill
+                    .corner_radius(egui::CornerRadius::same(1)) // optional rounded corners
+                    .show(ui, |ui| {
+                        ui.label("Cool Floating Area with some ui components and using egui::Frame for a border. again, egui::Area itself does not directly control its size — it wraps content and takes the size of whatever UI elements are inside it.");
+                        ui.heading("Hello, eframe + egui!");
+
+                        ui.horizontal(|ui| {
+                            ui.label("Enter your name:");
+                            ui.text_edit_singleline(&mut self.name_input);
+                        });
+
+                        ui.separator();
+
+                        if ui.button("Increment").clicked() {
+                            self.increment();
+                        }
+
+                        if ui.button("Reset").clicked() {
+                            self.reset();
+                        }
+
+                        if ui.button("PrintLn").clicked() {
+                            self.printx();
+                        }
+
+                        ui.label(format!("Counter value: {}", self.counter));
+
+                        if !self.name_input.is_empty() {
+                            ui.label(format!("Welcome, {}!", self.name_input));
+                        }
+                    });
+            });
+
+            egui::Area::new(egui::Id::new("my_area"))
+            .movable(true)
+            .default_pos(egui::pos2(50.0, 400.0))
+            .show(ctx, |ui| {
+                ui.label("Cool Floating Rustacean in an egui::Area. Areas are dragable, click the center and hold and drag area around to desired position. egui::Area itself does not directly control its size — it wraps content and takes the size of whatever UI elements are inside it.");
+                ui.add(
+                    egui::Image::new(egui::include_image!("../assets/Rustferris2.gif"))
+                        .fit_to_exact_size(egui::vec2(200.0, 200.0))
+                        .corner_radius(5),
+                );
+                ui.add(
+                    egui::Image::new(egui::include_image!("../assets/Rust_logo_animated1.gif"))
+                        .fit_to_exact_size(egui::vec2(200.0, 200.0))
+                        .corner_radius(5),
+                );
+            });
+
             ui.add(egui::github_link_file!(
                 "https://github.com/emilk/eframe_template/blob/main/",
                 "Source code."
             ));
+
+            // egui::Resize::default()
+            //     .id_salt("resizable_frame") // unique id
+            //     .default_size(self.size)
+            //     .show(ui, |ui| {
+            //         egui::Frame::new().fill(egui::Color32::RED).show(ui, |ui| {
+            //             ui.label("Label with red background");
+            //         });
+            //     });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn main() -> eframe::Result {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
+            .with_inner_size([1300.0, 1300.0])
+            .with_min_inner_size([700.0, 940.0])
             .with_icon(
                 // NOTE: Adding an icon is optional
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
@@ -18,9 +18,12 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "rust_eframe_egui",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Ok(Box::new(rust_eframe_egui::TemplateApp::new(cc)))
+        }),
     )
 }
 
@@ -50,7 +53,10 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| {
+                    egui_extras::install_image_loaders(&cc.egui_ctx);
+                    Ok(Box::new(rust_eframe_egui::TemplateApp::new(cc)))
+                }),
             )
             .await;
 
