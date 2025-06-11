a4c8c9f7a27ad01a3893436fafbd4718cd59c40c
RossF <rtbfletch@outlook.com>
2025-06-10 18:40:27 -0700

    added first_frame Boolean and set to TRUE in Default to set theme to light on startup, added syntax highlighting to Area 'code_area1', removed unused commented out code.

--- src/app.rs
+++ src/app.rs
@@ -6,11 +6,15 @@ pub struct TemplateApp {
     label: String,
     counter: i32,
     name_input: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
     x: i32,
     show_popup: bool,
+    first_frame: bool,
+    language: String,
+    code: String,
+
+    // This how you opt-out of serialization of a field
+    #[serde(skip)]
+    value: f32,
 }
 
 impl Default for TemplateApp {
@@ -25,6 +29,9 @@ impl Default for TemplateApp {
             name_input: String::new(),
             x: 5,
             show_popup: false,
+            first_frame: true,
+            language: "rs".into(),
+            code: String::new(),
         }
     }
 }
@@ -56,6 +63,25 @@ impl TemplateApp {
         let x = self.x;
         println!("the value of x is {}", x);
     }
+
+    // fn load_image_to_texture(ctx: &egui::Context, path: &str) -> egui::TextureHandle {
+    // let image = image::open(path).expect("Failed to load image");
+
+    // let size = [image.width() as usize, image.height() as usize];
+    // let image_buffer = image.to_rgba8(); // Converts to RGBA8
+
+    // let pixels = image_buffer
+    //     .pixels()
+    //     .map(|p| egui::Color32::from_rgba_unmultiplied(p[0], p[1], p[2], p[3]))
+    //     .collect();
+
+    // let color_image = egui::ColorImage {
+    //     size,
+    //     pixels,
+    // };
+
+    // ctx.load_texture("my_image", color_image, egui::TextureOptions::default())
+    // }
 }
 
 impl eframe::App for TemplateApp {
@@ -68,6 +94,11 @@ impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
+        if self.first_frame {
+            ctx.set_visuals(egui::Visuals::light());
+            self.first_frame = false;
+        }
+
         egui::SidePanel::left("side_nav")
             .exact_width(250.0)
             .show(ctx, |ui| {
@@ -135,6 +166,10 @@ Scalar type represents a single value. Rust has four primary scalar types: integ
                 });
         }
 
+        // if self.image.is_none() {
+        //     self.image = Some(self.load_image_to_texture(ctx, ""));
+        // }
+
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
@@ -175,21 +210,52 @@ Scalar type represents a single value. Rust has four primary scalar types: integ
 
             ui.separator();
 
-        egui::Area::new(egui::Id::new("code_area1"))
-            .movable(true)
-            // .default_pos(egui::pos2(850.0, 300.0))
-            .show(ctx, |ui| {
+        egui::Area::new(egui::Id::new("code_area1")).movable(true).show(ctx, |ui| {
                 ui.label("Drag and Reposition an Area using the 'Rustferris' image in the top left of each area, or anywhere else on an Area it seems! just not on text. The position of an area persists from where it was last dragged and positioned");
                 ui.add(
                     egui::Image::new(egui::include_image!("../assets/Rustferris2.gif"))
                         .fit_to_exact_size(egui::vec2(50.0, 50.0))
                         .corner_radius(5),
                 );
+
+        ui.horizontal(|ui| {
+                ui.label("Language:");
+                ui.text_edit_singleline(&mut self.language);
+            });
+        ui.horizontal_wrapped(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.label("Syntax highlighting powered by ");
+                ui.hyperlink_to("syntect", "https://github.com/trishume/syntect");
+                ui.label(".");
+
+            });
+
+        let mut theme = egui_extras::syntax_highlighting::CodeTheme::from_memory(ui.ctx(), ui.style());
+        ui.collapsing("Theme", |ui| {
+            ui.group(|ui| {
+                theme.ui(ui);
+                theme.clone().store_in_memory(ui.ctx());
+            });
+        });
+        let mut layouter = |ui: &egui::Ui, text: &str, wrap_width: f32| {
+            let mut layout_job = egui_extras::syntax_highlighting::highlight(
+                ui.ctx(),
+                ui.style(),
+                &theme,
+                text,
+                &mut self.language,
+            );
+            layout_job.wrap.max_width = wrap_width;
+            ui.fonts(|f| f.layout_job(layout_job))
+        };
+
+
                 ui.label("Here are Rust principles on Variables and Mutability & DataTypes in Rust");
                 let code = r#"
 //Variable and Mutability & DataTypes in Rust
 
 // This code would not compile, because by default, variables are immutable, and as the RustBook states, 'When a variable is immutable, once a value is bound to a name, you cant change that value.'
+
 fn main() {
     let x = 5;
     println!("The value of x is: {x}");
@@ -204,13 +270,13 @@ fn main() {
                         .code_editor() // Enables code styling
                         .desired_rows(13)
                         .lock_focus(true)
-                        .desired_width(400.0),
+                        .desired_width(f32::INFINITY)
+                        .layouter(&mut layouter),
                 );
             });
 
         egui::Area::new(egui::Id::new("my_area2"))
             .movable(true)
-            // .default_pos(egui::pos2(275.0, 400.0))
             .show(ctx, |ui| {
                 egui::Frame::new() // Start with no built-in padding/frame
                     .stroke(egui::Stroke::new(1.0, egui::Color32::ORANGE)) // border width and color
@@ -254,7 +320,6 @@ fn main() {
 
             egui::Area::new(egui::Id::new("my_area"))
             .movable(true)
-            // .default_pos(egui::pos2(275.0, 600.0))
             .show(ctx, |ui| {
                 ui.add(
                     egui::Image::new(egui::include_image!("../assets/Rustferris2.gif"))
@@ -274,15 +339,6 @@ fn main() {
                 "https://github.com/emilk/eframe_template/blob/main/",
                 "Source code."
             ));
-
-            // egui::Resize::default()
-            //     .id_salt("resizable_frame") // unique id
-            //     .default_size(self.size)
-            //     .show(ui, |ui| {
-            //         egui::Frame::new().fill(egui::Color32::RED).show(ui, |ui| {
-            //             ui.label("Label with red background");
-            //         });
-            //     });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
