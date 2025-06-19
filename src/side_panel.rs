6b378d8480b0bcb4090afc719614b4b6b1bb1f22
RossF <rtbfletch@outlook.com>
2025-06-19 05:15:01 -0700

    add SidePanelState struct to side_panel.rs

--- src/side_panel.rs
+++ src/side_panel.rs
@@ -1,29 +1,58 @@
 use crate::rustbook_code_blocks::{self};
 use eframe::egui;
 
-pub fn side_panel_ui(ctx: &egui::Context, spp1: &mut bool, language: &mut String) {
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct SidePanelState {
+    pub spp1: bool, // spp = side_panel_popup
+    pub spp2: bool,
+    pub language: String,
+}
+
+impl SidePanelState {
+    pub fn new() -> Self {
+        Self {
+            spp1: false,
+            spp2: false,
+            language: "rs".into()
+        }
+    }
+}
+
+pub fn side_panel_ui(ctx: &egui::Context, state: &mut SidePanelState) {
     egui::SidePanel::left("side_nav")
-        .exact_width(250.0)
+        .default_width(100.0)
+        // .exact_width(250.0)
         .show(ctx, |ui| {
-            ui.heading("Sidebar");
+            ui.heading("Common Rust Concepts");
             if ui.button("📦 Variables, Mutability & DataTypes").clicked() {
-                *spp1 = true;
+                if state.spp1 {
+                    state.spp1 = false;
+                } else {
+                    state.spp1 = true;
+                }
+            }
+            if ui.button("📦 Functions").clicked() {
+                if state.spp2 {
+                    state.spp2 = false;
+                } else {
+                    state.spp2 = true;
+                }
             }
         });
 
-    if *spp1 {
+    if state.spp1 {
         egui::Window::new("📦 Variables, Mutability & DataTypes").show(ctx, |ui| {
             ui.label("📦 Variables, Mutability & DataTypes");
             let theme = egui_extras::syntax_highlighting::CodeTheme::from_memory(ui.ctx(), ui.style());
             let code = rustbook_code_blocks::VARIABLES_MUTABILITY_DATATYPES;
-
             let mut layouter = |ui: &egui::Ui, text: &str, wrap_width: f32| {
                 let mut layout_job = egui_extras::syntax_highlighting::highlight(
                     ui.ctx(),
                     ui.style(),
                     &theme,
                     text,
-                    language,
+                    &state.language,
                 );
                 layout_job.wrap.max_width = wrap_width;
                 ui.fonts(|f| f.layout_job(layout_job))
@@ -44,7 +73,44 @@ pub fn side_panel_ui(ctx: &egui::Context, spp1: &mut bool, language: &mut String
                     );
                 });
             if ui.button("❌ Close").clicked() {
-                *spp1 = false;
+                state.spp1 = false;
+            }
+        });
+    }
+
+    if state.spp2 {
+        egui::Window::new("📦 Functions").show(ctx, |ui| {
+            ui.label("📦 Functions");
+            let theme = egui_extras::syntax_highlighting::CodeTheme::from_memory(ui.ctx(), ui.style());
+            let code = rustbook_code_blocks::FUNCTIONS;
+            let mut layouter = |ui: &egui::Ui, text: &str, wrap_width: f32| {
+                let mut layout_job = egui_extras::syntax_highlighting::highlight(
+                    ui.ctx(),
+                    ui.style(),
+                    &theme,
+                    text,
+                    &state.language,
+                );
+                layout_job.wrap.max_width = wrap_width;
+                ui.fonts(|f| f.layout_job(layout_job))
+            };
+
+            egui::ScrollArea::vertical()
+                .max_height(600.0) // Limit height for scrollability
+                .show(ui, |ui| {
+                    ui.add_sized(
+                        [ui.available_width(), 800.0], // Large height to simulate overflow
+                        egui::TextEdit::multiline(&mut code.to_string())
+                            .font(egui::TextStyle::Monospace) // Use monospaced font
+                            .code_editor() // Enables code styling
+                            .desired_rows(13)
+                            .lock_focus(true)
+                            .desired_width(f32::INFINITY)
+                            .layouter(&mut layouter),
+                    );
+                });
+            if ui.button("❌ Close").clicked() {
+                state.spp2 = false;
             }
         });
     }
